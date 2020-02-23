ef5f53e0c20d93a308eb54577a5564d6e229e0b7
SODA Chikashi <83107+chiqashi@users.noreply.github.com>
2020-02-23 23:01:02 +0900

    create a single view app project that supports iOS11

---
+++ iOS11SingleViewApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  iOS11SingleViewApp
+//
+//  Created by chikashi on 2020/02/23.
+//  Copyright © 2020 chikashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
